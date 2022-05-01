9ae4f2ac031357b8a273a0220f6548fdc058ef97
Litesh Garg <49069637+litesh1998@users.noreply.github.com>
2022-05-01 14:23:50 +0530

    Refactored reviews routes

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const mongoose = require('mongoose')
 const methodOveride = require('method-override')
 const ejsMate = require('ejs-mate')
 
-const Campground = require('./models/campground')
-const Review = require('./models/review')
-const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
-const {campgroundSchema, reviewSchema} = require('./schema')
 
 const campgrounds = require('./routes/campgrounds')
+const reviews = require('./routes/reviews')
 
 mongoose.connect('mongodb://root:example@localhost:27017/yelp-camp?authSource=admin&readPreference=primary&ssl=false');
 
@@ -20,29 +17,6 @@ db.once('open', ()=>{
     console.log("Database Connected")
 })
 
-const validateCampground = (req, res, next) =>{
-
-    const {error}= campgroundSchema.validate(req.body)
-    if (error){
-        const msg= error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    else{
-        next();
-    }
-    
-}
-
-const validateReview = (req,res, next)=>{
-    const {error}= reviewSchema.validate(req.body)
-    if (error){
-        const msg= error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    else{
-        next();
-    }
-}
 
 const app = express()
 app.engine('ejs', ejsMate)
@@ -52,28 +26,12 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOveride('_method'))
 
 
+app.use('/campgrounds/:id/reviews', reviews)
 app.use('/campgrounds',campgrounds)
-
 app.get('/', (req, res)=>{
     res.render('home')
 })
 
-
-
-app.post('/campgrounds/:id/reviews', validateReview ,catchAsync(async(req, res) =>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review)
-    campground.reviews.push(review)
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res)=>{
-    await Review.findByIdAndDelete(req.params.reviewId);
-    Campground.findByIdAndUpdate(req.params.id, {$pull: {reviews: req.params.reviewId}});
-    res.redirect(`/campgrounds/${req.params.id}`);
-}))
 
 app.all('*', (req, res, next)=>{
     next(new ExpressError('Page Not Found', 404))
---
+++ routes/reviews.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+const Review = require('../models/review');
+const Campground = require('../models/campground');
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError')
+const {reviewSchema} = require('../schema');
+
+
+const validateReview = (req,res, next)=>{
+    const {error}= reviewSchema.validate(req.body)
+    if (error){
+        const msg= error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    else{
+        next();
+    }
+}
+
+router.post('/', validateReview ,catchAsync(async(req, res) =>{
+    console.log(req.params.id)
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review)
+    campground.reviews.push(review)
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+router.delete('/:reviewId', catchAsync(async(req, res)=>{
+    await Review.findByIdAndDelete(req.params.reviewId);
+    Campground.findByIdAndUpdate(req.params.id, {$pull: {reviews: req.params.reviewId}});
+    res.redirect(`/campgrounds/${req.params.id}`);
+}))
+
+
+module.exports = router
