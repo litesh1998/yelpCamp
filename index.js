4104e726dc49a4b952814d1ac0ebe17e8533ee0b
Litesh Garg <49069637+litesh1998@users.noreply.github.com>
2022-05-14 20:02:17 +0530

    debuged

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("./error", { err });
 });
 
-const port = plrocess.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Serving on Port ${port}`);
